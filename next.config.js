e27868f44c0ab657d8c894f9e3cbd7293680cbf4
TheNaFeR02 <acunafer.02@gmail.com>
2023-12-23 18:43:57 -0500

    domain add next.config for images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['127.0.0.1'],
+    domains: ['127.0.0.1', 'guardians-api-dev.us-west-2.elasticbeanstalk.com'],
   },
   experimental: {
     serverActions: true,
